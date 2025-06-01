c51c1646929f46dac674b37563c14e483a471441
Burdon Jordan <burdon.jordan@gmail.com>
2025-06-01 22:56:23 +0200

    Save, started front setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import TopBar from "./components/TopBar/TopBar";
 import Footer from "./components/Footer/Footer";
+import { Outlet } from "react-router";
 
 function App() {
-	return (
-		<>
-			{/* si on a un context, il faudra mettre le provider là */}
-			<TopBar /> {/* les noms sont provisoires*/}
-			<main>
-				<Outlet />
-			</main>
-			<Footer />
-		</>
-	);
+    return (
+        <>
+            {/* si on a un context, il faudra mettre le provider là */}
+            <TopBar /> {/* les noms sont provisoires*/}
+            <main>
+                <Outlet />
+            </main>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
