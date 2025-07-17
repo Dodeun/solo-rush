e47f26661f7c0b93eed10ba2e2f89712a451b757
Burdon Jordan <burdon.jordan@gmail.com>
2025-07-17 17:28:08 +0200

    Started setuping Vitest for frontend

---
+++ client/src/test/setup.ts
@@ -0,0 +1,22 @@
+import { expect, afterEach } from "vitest";
+import { cleanup } from "@testing-library/react";
+import * as matchers from "@testing-library/jest-dom/matchers";
+
+// Extend Vitest's expect with jest-dom matchers
+expect.extend(matchers);
+
+// Without this you can use basic matchers like:
+// expect(element).toBe(something)
+// expect(element).toEqual(something)
+// expect(element).toBeTruthy()
+
+// With jest-dom matchers we can
+// expect(element).toBeInTheDocument()
+// expect(element).toHaveClass('active')
+// expect(element).toBeVisible()
+// expect(element).toHaveTextContent('Hi')
+
+// Cleanup after each test
+afterEach(() => {
+    cleanup();
+});
